1b23bda37ba9f7a2ff651ad89a200f3045873fa9
pwatts92 <pwatts92@gmail.com>
2016-02-11 20:34:42 -0500

    Update BurrowsWheeler.java

--- BurrowsWheeler.java
+++ BurrowsWheeler.java
@@ -1,76 +1,108 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 import edu.princeton.cs.algs4.BinaryStdIn;
 import edu.princeton.cs.algs4.BinaryStdOut;
 
 public class BurrowsWheeler {
-	
+
 	// apply Burrows-Wheeler transform, reading from standard input and writing
 	// to standard output
-	public static void transform(){
-		
+	public static void transform() {
 		String s = BinaryStdIn.readString();
-		//String s = "What in Davy Jones’ locker did ye just bark at me, ye scurvy bilgerat? I’ll have ye know I be the meanest cutthroat on the seven seas, and I’ve led numerous raids on fishing villages, and raped over 300 wenches. I be trained in hit-and-run pillaging and be the deadliest with a pistol of all the captains on the high seas. Ye be nothing to me but another source o’ swag. I’ll have yer guts for garters and keel haul ye like never been done before, hear me true. You think ye can hide behind your newfangled computing device? Think twice on that, scallywag. As we parley I be contacting my secret network o’ pirates across the sea and yer port is being tracked right now so ye better prepare for the typhoon, weevil. The kind o’ monsoon that’ll wipe ye off the map. You’re sharkbait, fool. I can sail anywhere, in any waters, and can kill ye in o’er seven hundred ways, and that be just with me hook and fist. Not only do I be top o’ the line with a cutlass, but I have an entire pirate fleet at my beck and call and I’ll damned sure use it all to wipe yer arse off o’ the world, ye dog. If only ye had had the foresight to know what devilish wrath your jibe was about to incur, ye might have belayed the comment. But ye couldn’t, ye didn’t, and now ye’ll pay the ultimate toll, you buffoon. I’ll shit fury all over ye and ye’ll drown in the depths o’ it. You’re fish food now.";
-		
+
 		CircularSuffixArray transArray = new CircularSuffixArray(s);
+
 		
-		String transString = "";
-		for (int i = 0; i < transArray.length(); i++){
-			transString = transString + CircularSuffixArray.circularSuffixArray[i].charAt(transArray.length()-1);
+		char[] transChars = new char[s.length()];
+		for (int i = 0; i < transArray.length(); i++) {
+			transChars[i] = CircularSuffixArray.circularSuffixArray[i].charAt(transArray.length() - 1);
 		}
-		transString = transArray.index(0) + " " + transString;
-		
-		//System.out.println(transString);
+		int index = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (transArray.index(i) == 0) {
+				index = i;
+				break;
+			}
+		}
+
+		String transString = index + new String(transChars);
 		BinaryStdOut.write(transString);
 	}
 
-    // apply Burrows-Wheeler inverse transform, reading from standard input and writing to standard output
-	public static void inverseTransform(){
+	// apply Burrows-Wheeler inverse transform, reading from standard input and
+	// writing to standard output
+	public static void inverseTransform() {
+		int originalIndex = BinaryStdIn.readInt();
 		String wheelerString = BinaryStdIn.readString();
-		
-		String indexString = "";
-		int i = 0;
-		while(wheelerString.charAt(i) != ' '){
-			indexString = indexString + wheelerString.charAt(i);
-			i++;
-		}
-		//Index output by a Burrows-wheeler Transform
-		int originalIndex = Integer.parseInt(indexString);
-		
-		//String output by a Burrows-wheeler Transform, minus the index
-		wheelerString = wheelerString.substring(i);
-		
-		//Sorts the wheelerString to produce the first column of the circular suffix array
+
+		// Sorts the wheelerString to produce the first column of the circular
+		// suffix array
 		char[] c = wheelerString.toCharArray();
+
 		Arrays.sort(c);
 		String sortedString = new String(c);
-		
-		//Constructs the next array, which is a recreation of the index array from BWT
+
+		// Constructs the next array, which is a recreation of the index array
 		int[] next = new int[wheelerString.length()];
-		
-		//Fills in the next array
-		//TODO
-		
-		//Reconstructs the original string using the next array.
-		String originalString = "" + sortedString.charAt(originalIndex);
-		int x = originalIndex;
-		while(originalString.length() < wheelerString.length() - 1){
-			x = next[x];
-			originalString = originalString + sortedString.charAt(x);
+
+		// Fills in the next array. Uses a Hashmap to contain the positions of
+		// characters.
+		// Hashmap uses iterations of a character as a key, and an arraylist of
+		// the int positions of those characters . The arraylist goes from Lower
+		// -> Higher.
+		HashMap<Character, ArrayList<Integer>> wheelerMap = new HashMap<Character, ArrayList<Integer>>();
+		ArrayList<Integer> arrayPointer = null;
+
+		for (int i = 0; i < wheelerString.length(); i++) {
+			if (!wheelerMap.containsKey(wheelerString.charAt(i))) {
+				arrayPointer = new ArrayList<Integer>();
+				arrayPointer.add(i);
+				wheelerMap.put(wheelerString.charAt(i), arrayPointer);
+				arrayPointer = null;
+			} else {
+				arrayPointer = wheelerMap.get(wheelerString.charAt(i));
+				for (int x = 0; x < arrayPointer.size(); x++)
+					if (i < arrayPointer.get(x)) {
+						arrayPointer.add(i, x);
+						break;
+					} else if (x == arrayPointer.size() - 1) {
+						arrayPointer.add(i);
+						break;
+					}
+				arrayPointer = null;
+			}
 		}
-		originalString = originalString + wheelerString.charAt(originalIndex);
+
+		// Fills in the next array using the hashmap's smallest value for a
+		// given character.
+		// Since we start at 0, the sorted character will also have the smallest
+		// possible value.
+		for (int i = 0; i < sortedString.length(); i++) {
+			next[i] = wheelerMap.get(sortedString.charAt(i)).remove(0);
+		}
+
+		// Reconstructs the original string using the next array.
+		char[] originalString = new char[sortedString.length()];
+		originalString[0] = sortedString.charAt(originalIndex);
+		originalString[sortedString.length()-1] = wheelerString.charAt(originalIndex);
 		
-		BinaryStdOut.write(originalString);
+		int x = originalIndex;
+		for(int charIndex = 1; charIndex < wheelerString.length() - 1; charIndex++) {
+			x = next[x];
+			originalString[charIndex] = sortedString.charAt(x);
+		}
+
+		BinaryStdOut.write(new String(originalString));
 	}
 
-    // if args[0] is '-', apply Burrows-Wheeler transform
-    // if args[0] is '+', apply Burrows-Wheeler inverse transform
-	public static void main(String[] args){
-		//if(args[0] == "-")
+	// if args[0] is '-', apply Burrows-Wheeler transform
+	// if args[0] is '+', apply Burrows-Wheeler inverse transform
+	public static void main(String[] args) {
+		if (args[0] == "-")
 			transform();
-		/*else if(args[0] == "+")
+		else if (args[0] == "+")
 			inverseTransform();
-		else
-			transform();*/
 	}
 }
